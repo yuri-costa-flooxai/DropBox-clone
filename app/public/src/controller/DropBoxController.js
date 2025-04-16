fa8578abe794e91ffb30942127965971d4a324f3
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-16 11:55:38 -0300

    feat: deleting files & folders from firebase

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -43,6 +43,48 @@ class DropBoxController {
     return this.listFilesEl.querySelectorAll(".selected");
   }
 
+  removeFolderTask(ref, name) {
+    return new Promise((resolve, reject) => {
+      let folderRef = this.getFirebaseRef(ref + '/' + name);
+
+      folderRef.on('value', snapshot => {
+
+        folderRef.off('value');
+
+        snapshot.forEach(item => {
+
+          let data = item.val();
+          data.key = item.key;
+
+          if (data.type === 'folder') {
+            this.removeFolderTask(ref + '/' + name, data.name).then(() => {
+              resolve({
+                fields: {
+                  key: data.key
+                }
+              })
+            }).catch(err => {
+              reject(err);
+            });
+          } else if (data.type) {
+            this.removeFile(ref + '/' + name, data.name).then(() => {
+              resolve({
+                fields: {
+                  key: data.key
+                }
+              })
+            }).catch(err => {
+              reject(err);
+            })
+          }
+
+        })
+
+        folderRef.remove();
+      })
+    })
+  }
+
   removeTask() {
     let promises = [];
 
@@ -50,16 +92,40 @@ class DropBoxController {
       let file = JSON.parse(li.dataset.file);
       let key = li.dataset.key;
 
-      let formData = new FormData()
+      promises.push(new Promise((resolve, reject) => {
 
-      formData.append('path', file.path);
-      formData.append('key', key);
+        if (file.type === 'folder') {
 
-      promises.push(this.ajax('/file', 'DELETE', formData));
+          this.removeFolderTask(this.currentFolder.join('/'), file.name).then(() => {
+            resolve({
+              fields: {
+                key
+              }
+            });
+          });
+
+        } else if (file.type) {
+          this.removeFile(this.currentFolder.join('/'), file.name).then(() => {
+            resolve({
+              fields: {
+                key
+              }
+            });
+          });
+        }
+
+      }));
 
     });
     
     return Promise.all(promises);
+  }
+
+  removeFile(ref, name) {
+    let fileRef = firebase.storage().ref(ref).child(name);
+
+    return fileRef.delete();
+      
   }
 
   initEvents() {
@@ -134,7 +200,12 @@ class DropBoxController {
       this.uploadTask(event.target.files)
         .then((responses) => {
           responses.forEach((resp) => {
-            this.getFirebaseRef().push().set(resp.files["input-file"]);
+            this.getFirebaseRef().push().set({
+              name: resp.name,
+              type: resp.contentType,
+              path: resp.downloadURLs[0],
+              size: resp.size
+            });
           });
 
           this.uploadComplete();
@@ -201,23 +272,29 @@ class DropBoxController {
     let promises = [];
 
     [...files].forEach((file) => {
-      let formData = new FormData();
+      promises.push(new Promise((resolve, reject) => {
+        let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
+      
+        let task = fileRef.put(file);
+  
+        task.on('state_changed', snapshot => {
+          this.uploadProgress({
+            loaded: snapshot.bytesTransferred,
+            total: snapshot.totalBytes
+          }, file);
+        }, error => {
+          console.error(error);
+          reject(error);
+        }, () => {
 
-      formData.append("input-file", file);
+          fileRef.getMetadata().then(metadata => {
+            resolve(metadata);
+          }).catch(err => {
+            reject(err);
+          })
 
-      promises.push(
-        this.ajax(
-          "/upload",
-          "POST",
-          formData,
-          () => {
-            this.uploadProgress(event, file);
-          },
-          () => {
-            this.startUploadTime = Date.now();
-          }
-        )
-      );
+        });
+      }));
     });
 
     return Promise.all(promises);
